f8a263ed62c929cf9c45fbeb3dc0737e7fd4c8d5
Santosh <santosharakere@gmail.com>
2017-04-29 04:25:16 +0100

    updating the IP of influx db

--- commands.sh
+++ commands.sh
@@ -2,3 +2,11 @@ sudo service docker status
 sudo service docker start
 sudo service docker stop
 docker run -it ubuntu /bin/bash
+
+
+White Horse Surveyors Ltdsudo 
+apt-get update
+sudo apt-get install influxdb-client
+
+
+influx -execute 'SHOW DATABASES'
